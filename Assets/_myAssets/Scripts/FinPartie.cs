816b887d1f9aaa3b017df4eb31d7cce5dc1a8abe
MasterPlayerOfficial <46854057+MasterPlayerOfficial@users.noreply.github.com>
2023-03-21 15:41:30 -0400

    Update

--- Assets/_myAssets/Scripts/FinPartie.cs
+++ Assets/_myAssets/Scripts/FinPartie.cs
@@ -23,19 +23,40 @@ public class FinPartie : MonoBehaviour
             _finPartie = true;
             int noScene = SceneManager.GetActiveScene().buildIndex;
 
-            if (noScene == 2)
+            if (noScene == 0)
+            {
+                _gestionJeu.SetNiveau1(_gestionJeu.GetPointage(), Time.time);
+                SceneManager.LoadScene(noScene + 1);
+                _finPartie = false;
+            }
+            else if (noScene == 1)
+            {
+                _gestionJeu.SetNiveau2((_gestionJeu.GetPointage() - _gestionJeu.GetAccrochagesNiveau1()), (Time.time - _gestionJeu.GetTempsNiveau1()));
+                SceneManager.LoadScene(noScene + 1);
+                _finPartie = false;
+            }
+            else if (noScene == 2)
             {
                 int accrochages = _gestionJeu.GetPointage();
-                Debug.Log("Fin de partie!");
-                Debug.Log("Le temps est de "  + Time.time + " secondes");
-                Debug.Log("Vous avez accoché " + accrochages + " obstacles");
-                float tempsTotal = Time.time + accrochages;
-                Debug.Log("Votre temps total est de : " + tempsTotal);
+                float _tempsTotalNiveau1 = _gestionJeu.GetTempsNiveau1() + _gestionJeu.GetAccrochagesNiveau1();
+                float _tempsTotalNiveau2 = _gestionJeu.GetTempsNiveau2() + _gestionJeu.GetAccrochagesNiveau2();
+                float _tempsNiveau3 = Time.time - (_gestionJeu.GetTempsNiveau1() + _gestionJeu.GetTempsNiveau2());
+                int _accrochagesNiveau3 = _gestionJeu.GetPointage() - (_gestionJeu.GetAccrochagesNiveau1() + _gestionJeu.GetAccrochagesNiveau2());
+                float _tempsTotalNiveau3 = _tempsNiveau3 + _accrochagesNiveau3;
+
+                Debug.Log("Fin de la partie!");
+                Debug.Log("Le temps pour le niveau 1 est de "  + _gestionJeu.GetTempsNiveau1().ToString("f2") + " secondes");
+                Debug.Log("Vous avez accoché au niveau 1 " + _gestionJeu.GetAccrochagesNiveau1() + " obstacles");
+                Debug.Log("Votre temps total est de : " + _tempsTotalNiveau1.ToString("f2") + "secondes");
+                Debug.Log("Le temps pour le niveau 2 est de "  + _gestionJeu.GetTempsNiveau2().ToString("f2") + " secondes");
+                Debug.Log("Vous avez accoché au niveau 2 " + _gestionJeu.GetAccrochagesNiveau2() + " obstacles");
+                Debug.Log("Votre temps total est de : " + _tempsTotalNiveau2.ToString("f2") + "secondes");
+                Debug.Log("Le temps pour le niveau 3 est de "  + _tempsNiveau3.ToString("f2") + " secondes");
+                Debug.Log("Vous avez accoché au niveau 3 " + _accrochagesNiveau3 + " obstacles");
+                Debug.Log("Votre temps total est de : " + _tempsTotalNiveau3.ToString("f2") + "secondes");
+                Debug.Log("Votre temps total pour les trois niveaux est de" + (_tempsTotalNiveau1 + _tempsTotalNiveau2 + _tempsTotalNiveau3).ToString("f2") + "secondes");
+
                 _player.finPartieJoueur();
-            }
-            else
-            {
-                SceneManager.LoadScene(noScene + 1);
             }
         }
     }
